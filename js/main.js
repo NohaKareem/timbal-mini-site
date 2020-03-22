fb1c190d71c5a38e151dd1c923b09ce4a3f34e54
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-03-22 10:30:19 -0400

    Applied soft UI buttons

--- js/main.js
+++ js/main.js
@@ -21,4 +21,63 @@
 		navLink.addEventListener("click", preventDefaultBehavior);
 	});
 
+	function animateText() {
+		console.log('~in animateText')
+		let customVarsAnimatedText = document.querySelector('#customVarsAnimatedText');
+		let systemsAnimatedText = document.querySelector('#systemsAnimatedText');
+
+		let customVarsText = ['read.', 'spend quality time.', 'write.', 'workout.', 'bake.'];
+
+		//~ using space delimter to replace word by word (as opposed to char by char)
+		var tl = new TimelineMax();//{ repeat: (this.count - 1) });
+
+		// animation for custom variables feature
+		// tl.to(customVarsAnimatedText, 10,{ text: {value: "hi"}, ease: "Power0.easeIn"});
+		// ~gsap.config({nullTargetWarn:false});//https://greensock.com/forums/topic/22836-gsap-3-warnings/
+		tl.to('#customVarsAnimatedText', {
+			duration: 2,
+			text: {
+				value: "This is the new text",
+				delimiter: " "
+			},
+			ease: "none"
+		});
+		// customVarsText.forEach(textToAnimate => {
+		// 	tl.to(customVarsAnimatedText, {
+		// 		duration: 10, 
+		// 		text: {
+		// 			value: textToAnimate
+		// 		}
+		// 		, ease: Power0.easeIn
+		// 	});
+		// });
+
+		// tl.to(circle, this.inhale,
+		// 	{
+		// 		text: {
+		// 			value: "inhale",
+		// 			// delimiter: " ",
+		// 		}, ease: Power0.easeIn
+		// 	});
+			// hold
+			// .to(circle, this.hold,
+			// 	{
+			// 		text: {
+			// 			value: "hold",
+			// 			delimiter: " ",
+			// 		}, ease: Power0.linear
+			// 	})
+			// // exhale
+			// .to(circle, this.exhale,
+			// 	{
+			// 		text: {
+			// 			value: "exhale",
+			// 			delimiter: " ",
+			// 		}, ease: Power0.easeOut
+			// 	})
+
+			// reset text
+			// .to(circle, 0.01, { text: { value: " ", delimiter: " " }, ease: Power0.easeIn });
+	}
+	animateText();
 })();
